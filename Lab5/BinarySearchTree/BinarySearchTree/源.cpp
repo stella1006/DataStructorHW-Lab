fbe18dfc0b2a223a7d85d2b57fe32cafab7ff02e
stella1006 <stella.lee.lzr@gmail.com>
2016-05-09 17:53:48 +0800

    modify binTree

--- Lab5/BinarySearchTree/BinarySearchTree/源.cpp
+++ Lab5/BinarySearchTree/BinarySearchTree/源.cpp
@@ -3,14 +3,22 @@
 #include <ctime>
 using namespace std;
 
+template <typename T>
+void visit(T d) {
+	cout << d << " ";
+}
+
 int main() {
-	srand(time(0));
+	srand((unsigned int)time(0));
 	BinTree<int> tree;
+	//BinTree<int> tree2(tree1);
 	for (int i = 0; i < 20; i++) {
 		tree.insert(rand() % 15);
 	}
+	cout << "Level traverse:" << endl;
 	tree.print();
-	tree.traversal();
+	cout << "Inordered traverse:" << endl;
+	tree.traversal(visit);
 
 	system ("pause");
 	return 0;
