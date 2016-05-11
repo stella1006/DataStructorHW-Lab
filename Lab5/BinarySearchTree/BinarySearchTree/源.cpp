e0a57d1c8cc1fd50755fc7ef3edc9ae08736d56b
stella1006 <stella.lee.lzr@gmail.com>
2016-05-11 22:18:05 +0800

    modify Bintree

--- Lab5/BinarySearchTree/BinarySearchTree/源.cpp
+++ Lab5/BinarySearchTree/BinarySearchTree/源.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+const int N = 20; //number of datas
+const int MAX = 20; //max value of the data
 
 template <typename T>
 void visit(T d) {
@@ -11,15 +13,16 @@ void visit(T d) {
 int main() {
 	srand((unsigned int)time(0));
 	BinTree<int> tree;
-	//BinTree<int> tree2(tree1);
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 20; i++) {   //generate N random numbers and insert them into the tree one by one
 		tree.insert(rand() % 15);
 	}
 	cout << "Level traverse:" << endl;
 	tree.print();
 	cout << "Inordered traverse:" << endl;
 	tree.traversal(visit);
-
+	cout << endl;
+	int d = rand() % MAX; //generate a random number and search it in the tree
+	cout << "Search an element: " << d << " " << (tree.search(d) == 1 ? "true" : "flase") << endl;
 	system ("pause");
 	return 0;
 }
